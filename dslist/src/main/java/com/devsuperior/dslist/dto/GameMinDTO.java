f87b9fc62603f80e1a6de91a4edf3bee6853938f
RuanSampaio-code <ruansampaio001@gmail.com>
2024-11-15 17:37:49 -0300

    gameRepository, dto, controller

---
+++ dslist/src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dslist.dto;
+
+
+import com.devsuperior.dslist.entities.Game;
+import lombok.Getter;
+
+
+@Getter
+public class GameMinDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO(){
+    }
+
+    public GameMinDTO(Game entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.year = entity.getYear();
+        this.imgUrl = entity.getImgUrl();
+        this.shortDescription = entity.getShortDescription();
+    }
+
+}
